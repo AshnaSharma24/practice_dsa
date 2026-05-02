0cb9975729c91295e775a434fa007bed908f717e
AshnaSharma24 <ashna24sharma@gmail.com>
2026-05-02 19:53:38 +0530

    Stack

---
+++ 150. Evaluate Reverse Polish Notation.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        for(String t : tokens){
+            if(t.equals("+") || t.equals("-") || t.equals("*") ||t.equals("/")){
+                int b = st.pop();
+                int a = st.pop();
+                switch(t){
+                    case "+" : 
+                    st.push(a+b);
+                    break;
+                    case "-" : 
+                    st.push(a-b);
+                    break;
+                    case "*" : 
+                    st.push(a*b);
+                    break;
+                    case "/" : 
+                    st.push(a/b);
+                    break;
+                }
+            }
+            else st.push(Integer.parseInt(t));
+        }
+        return st.pop();
+    }
+}
